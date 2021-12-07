fbafcd993e33d470ffece243be2bc80733adba98
Kristopher Kieft <kristopher@Kristophers-MacBook-Pro.local>
2021-12-07 14:04:06 -0600

    v1.1.0 major update

---
+++ setup.py
@@ -0,0 +1,44 @@
+#! /usr/bin/env python3
+# PropagAtE
+# Author: Kristopher Kieft
+# University of Wisconsin-Madison
+
+import setuptools
+
+def get_descript():
+    with open("README.md", "r") as dfile:
+        d = dfile.read()
+    return d
+
+def get_version():
+    with open("VERSION", 'r') as vfile:
+        v = vfile.readline().strip()
+    return v
+
+def do_setup(v, d):
+    setuptools.setup(
+        name="PropagAtE",
+        version=v,
+        author="Kristopher Kieft",
+        author_email="kieft@wisc.edu",
+        description="PropagAtE: Prophage Activity Estimator",
+        long_description_content_type="text/markdown",
+        url="https://github.com/AnantharamanLab/PropagAtE",
+        classifiers=[
+        "Programming Language :: Python :: 3",
+        "Topic :: Scientific/Engineering",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: GNU General Public License (GPLv3)"
+        ],
+        install_requires=['numpy>=1.17.0', 'pysam>=0.15', 'numba>=0.50.0'],
+        keywords=["bioinformatics", "metagenomics", "virus", "prophage"],
+        python_requires=">=3.6",
+        scripts=['Propagate/PropagAtE', 'Propagate/modules.py'],
+        packages=setuptools.find_packages(),
+        license='GPLv3',
+        long_description=d
+    )
+
+
+if __name__ == "__main__":
+    do_setup(get_version(), get_descript())
